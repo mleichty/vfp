41b36e8288ad34a92a48aed598e9940aa33c21c4
mleichty <marialeichty4@gmail.com>
2020-07-09 18:56:30 -0400

    Updates

---
+++ precache-manifest.cd8e34104ce5a09a6c2e31b92fc3d66d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0ef3da79feec230d5c86954154c385",
+    "url": "/vfp/index.html"
+  },
+  {
+    "revision": "88db26254ca49ea505db",
+    "url": "/vfp/static/css/main.79086240.chunk.css"
+  },
+  {
+    "revision": "ab2aec343fb75b718ebd",
+    "url": "/vfp/static/js/2.dfbc89fc.chunk.js"
+  },
+  {
+    "revision": "3549e0d2c67b94c5a56545755a940411",
+    "url": "/vfp/static/js/2.dfbc89fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88db26254ca49ea505db",
+    "url": "/vfp/static/js/main.f5e7510e.chunk.js"
+  },
+  {
+    "revision": "77870c9c947575a65e8f",
+    "url": "/vfp/static/js/runtime-main.e7938268.js"
+  },
+  {
+    "revision": "7096aaabe08f3a29a72ddbc9ba6bcb69",
+    "url": "/vfp/static/media/GloberBook.7096aaab.otf"
+  },
+  {
+    "revision": "d368ef367981da5e5a397efdb5787450",
+    "url": "/vfp/static/media/GloberSemiBold.d368ef36.otf"
+  }
+]);
